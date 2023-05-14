998a9f7f0248a98a62d75e49078adce8733df2c3
Paolo Mainardi <paolomainardi@gmail.com>
2023-05-14 17:19:09 +0000

    feat: change image with a real one as suggested

--- scripts/convert-png-to-webp.sh
+++ scripts/convert-png-to-webp.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 docker run -v "$PWD"/src/static/images:/imgs --entrypoint bash dpokidov/imagemagick:7.1.0-55-ubuntu -c "mogrify -format webp /imgs/posts/**/*.png"
+docker run -v "$PWD"/src/static/images:/imgs --entrypoint bash dpokidov/imagemagick:7.1.0-55-ubuntu -c "mogrify -format webp /imgs/posts/**/*.jpg"
